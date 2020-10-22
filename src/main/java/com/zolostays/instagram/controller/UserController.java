22ea8e4789ef958c2d040b516b1a01402aed28d5
Pankaj Singh <pankaj.ks@zolostays.com>
2020-10-22 18:31:46 +0530

    Added logger

--- src/main/java/com/zolostays/instagram/controller/UserController.java
+++ src/main/java/com/zolostays/instagram/controller/UserController.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import static net.logstash.logback.argument.StructuredArguments.*;
-import java.util.LinkedList;
 import java.util.Optional;
 
 @RestController
@@ -25,10 +24,12 @@ public class UserController {
 
     @PostMapping
     public ResponseDTO<UserDTO> addUser(@RequestBody UserDTO userDTO){
+        logger.info("Got a request to Create User");
         Optional<UserDTO> userDTOOptional = userService.addUser(userDTO);
         if(userDTOOptional.isPresent()){
             return Mapper.responseDTOSingle(userDTOOptional, "User Created" );
         }
+        logger.error("Username already Taken");
         return Mapper.responseDTOSingle(null, "Username Already Taken");
     }
 
@@ -37,26 +38,28 @@ public class UserController {
         logger.info("User Requested for getting user with id: {}" ,id);
         Optional<UserDTO> userDTOOptional  = userService.getUser(id);
         if(userDTOOptional.isPresent()){
-            logger.info("User Recieved", kv("user", userDTOOptional.get()));
             return Mapper.responseDTOSingle(userDTOOptional, "You have got User" );
         }
-        logger.error("User doesn't exist with {}", kv("userId", id));
         return Mapper.responseDTOSingle(null, "User Doesn't exist of given ID");
     }
 
     @PutMapping("/{id}")
     public ResponseDTO updateUser(@RequestBody UserDTO userDTO, @PathVariable("id") Long user_id){
+        logger.info("User Requested for updating with {}", kv("userID", user_id));
         Optional<UserDTO> userDTOOptional  = userService.updateUser(userDTO, user_id);
         if(userDTOOptional.isPresent()){
+            logger.info("User Updated with {}", kv("userId", user_id));
             return Mapper.responseDTOSingle(userDTOOptional, "User updated" );
         }
-        return Mapper.responseDTOSingle(null, "User didn't get updated");
+        logger.error("Failed to update User with {}", kv("userID", user_id));
+        return Mapper.responseDTOSingle(null, "Failed to update User");
     }
 
     @DeleteMapping("/{id}")
-    public ResponseDTO deleteUser(@PathVariable("id") Long id){
+    public ResponseDTO deleteUser(@PathVariable("id") Long user_id){
+        logger.info("Requested for deleting usere with {}", kv("userID", user_id));
         try{
-            userService.deleteUser(id);
+            userService.deleteUser(user_id);
             return Mapper.objectDeleted("User deleted");
         }catch (UserDoesNotExistException e) {
             return Mapper.responseDTOSingle(null, e.getMessage());
--- src/main/java/com/zolostays/instagram/service/UserServiceImpl.java
+++ src/main/java/com/zolostays/instagram/service/UserServiceImpl.java
@@ -5,13 +5,19 @@ import com.zolostays.instagram.exception.UserDoesNotExistException;
 import com.zolostays.instagram.model.User;
 import com.zolostays.instagram.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Optional;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
 
 @Component
 public class UserServiceImpl implements IUserService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private UserRepository userRepository;
@@ -25,23 +31,27 @@ public class UserServiceImpl implements IUserService {
         if(optionalUser.isPresent()){
             User user = optionalUser.get();
             UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+            logger.info("User Recieved", kv("user", userDTO));
             return Optional.of(userDTO);
         }else{
+            logger.error("User doesn't exist with {}", kv("userId", id));
             return Optional.empty();
         }
     }
 
     @Override
-    public Optional<User> getUserById(Long id){
-        return userRepository.findById(id);
+    public Optional<User> getUserById(Long user_id){
+        return userRepository.findById(user_id);
     }
 
     @Override
-    public void deleteUser(Long id) throws UserDoesNotExistException{
-        if(userRepository.existsById(id)){
-            userRepository.deleteById(id);
+    public void deleteUser(Long user_id) throws UserDoesNotExistException{
+        if(userRepository.existsById(user_id)){
+            userRepository.deleteById(user_id);
+            logger.info("User Deleted with {}", kv("userID", user_id));
         }else{
-            throw new UserDoesNotExistException(id.toString());
+            logger.error("User doesn't exist with given {}", kv("userID", user_id));
+            throw new UserDoesNotExistException(user_id.toString());
         }
 
     }
@@ -52,8 +62,10 @@ public class UserServiceImpl implements IUserService {
         if(!userRepository.existsByUsername(user.getUsername())) {
             user = userRepository.save(user);
             UserDTO resultUserDTO = modelMapper.map(user, UserDTO.class);
+            logger.info("User created", kv("userDTO", resultUserDTO));
             return Optional.of(resultUserDTO);
         }else{
+            logger.error("Failed To create User",kv("userDTO", userDTO) );
             return Optional.empty();
         }
     }
