2be88f6f2dc53a951e47cd4c16f1a5231a932353
Pankaj <pankaj.ks@zolostays.com>
2020-10-29 21:51:39 +0530

    Added

--- src/test/java/com/zolostays/instagram/service/UserServiceUnitTest.java
+++ src/test/java/com/zolostays/instagram/service/UserServiceUnitTest.java
@@ -7,9 +7,12 @@ import com.zolostays.instagram.model.User;
 import com.zolostays.instagram.repository.UserRepository;
 import com.zolostays.instagram.sampleData.userData.UserData;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
@@ -18,7 +21,8 @@ import static org.mockito.BDDMockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class UserServiceUnitTest {
 
 
